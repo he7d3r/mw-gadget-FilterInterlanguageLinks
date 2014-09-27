fc8a95b94ecc4cffa1afa040775abc60b2217231
Helder <he7d3r@gmail.com>
2014-09-27 20:48:59 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/FilterInterlanguageLinks.js
+++ src/FilterInterlanguageLinks.js
@@ -4,48 +4,48 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
-var enMsgs = {
-	'fil-show': 'Show more $1',
-	'fil-desc': 'Toggle link(s) which do not appear by default',
-	'fil-hide': 'Hide $1'
-};
-mw.messages.set( $.extend( {}, enMsgs, window.filterInterlanguageLinks && window.filterInterlanguageLinks.msgs ) );
-function customize (){
-	var // [[meta:List of Wikipedias#1 000 000+ articles]]
-		top = [ 'de', 'en', 'es', 'fr', 'it', 'nl', 'pl', 'ru', 'sv' ],
-		langsToShow = window.filterInterlanguageLinks && window.filterInterlanguageLinks.show || top,
-		sel = '.interwiki-' + langsToShow.join( ', .interwiki-' ) + ', .wbc-editpage',
-		count = $( '#p-lang' )
-			.find( 'li' )
-			.filter( function(){
-				return ! $( this ).is( sel );
-			} )
-			.addClass( 'interwiki-hidden-by-default' )
-			.hide()
-			.length;
-	if( !count ){
-		return;
+	'use strict';
+	var enMsgs = {
+		'fil-show': 'Show more $1',
+		'fil-desc': 'Toggle link(s) which do not appear by default',
+		'fil-hide': 'Hide $1'
+	};
+	mw.messages.set( $.extend( {}, enMsgs, window.filterInterlanguageLinks && window.filterInterlanguageLinks.msgs ) );
+	function customize() {
+		var // [[meta:List of Wikipedias#1 000 000+ articles]]
+			top = [ 'de', 'en', 'es', 'fr', 'it', 'nl', 'pl', 'ru', 'sv' ],
+			langsToShow = window.filterInterlanguageLinks && window.filterInterlanguageLinks.show || top,
+			sel = '.interwiki-' + langsToShow.join( ', .interwiki-' ) + ', .wbc-editpage',
+			count = $( '#p-lang' )
+				.find( 'li' )
+				.filter( function () {
+					return !$( this ).is( sel );
+				} )
+				.addClass( 'interwiki-hidden-by-default' )
+				.hide()
+				.length;
+		if ( !count ) {
+			return;
+		}
+		$( mw.util.addPortletLink(
+			'p-lang',
+			'#',
+			mw.msg( 'fil-show', count ),
+			'p-show-ilw',
+			mw.msg( 'fil-desc' ),
+			null,
+			$('#p-lang li:last')
+		) ).click( function ( e ) {
+			var isVisible = $( '.interwiki-hidden-by-default' ).toggle().is( ':visible' );
+			$( e.target ).text(
+				isVisible ? mw.msg( 'fil-hide', count ) : mw.msg( 'fil-show', count )
+			);
+			e.preventDefault();
+		} ).addClass( 'fil-toggle' );
 	}
-	$( mw.util.addPortletLink(
-		'p-lang',
-		'#',
-		mw.msg( 'fil-show', count ),
-		'p-show-ilw',
-		mw.msg( 'fil-desc' ),
-		null,
-		$('#p-lang li:last')
-	) ).click( function( e ){
-		var isVisible = $( '.interwiki-hidden-by-default' ).toggle().is( ':visible' );
-		$( e.target ).text(
-			isVisible ? mw.msg( 'fil-hide', count ) : mw.msg( 'fil-show', count )
-		);
-		e.preventDefault();
-	} ).addClass( 'fil-toggle' );
-}
 
-if( mw.user.options.get('uls-compact-links') !== '1' ){
-	$( customize );
-}
+	if ( mw.user.options.get('uls-compact-links') !== '1' ) {
+		$( customize );
+	}
 
 }( mediaWiki, jQuery ) );
